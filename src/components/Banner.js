b85cdbf3c6b517898b16d6269110dd69a3dc4f56
nwj6535 <nwj6535@gmail.com>
2023-09-14 20:00:08 +0900

    feat: Add banner UI

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "../api/axios";
+import "./Banner.css";
 
 // Local imports
 import requests from "../api/request";
@@ -29,7 +30,34 @@ const Banner = () => {
     setMovie(movieDetail);
   };
 
-  return <div>Banner</div>;
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substring(0, n) + "..." : str;
+  };
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+        backgroundPosition: "top cetner",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie.title || movie.name || movie.original_name}
+        </h1>
+
+        <div className="banner__buttons">
+          {movie?.videos?.results[0]?.key && (
+            <button className="banner__button play">Play</button>
+          )}
+        </div>
+        <p className="banner__description">{truncate(movie.overview, 100)}</p>
+      </div>
+      <div className="banner--fadeBottom" />
+    </header>
+  );
 };
 
 export default Banner;
