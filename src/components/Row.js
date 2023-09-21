9c6d99bfa9b12539e8dd5dff626dd15ea1cfaabb
nwj6535 <nwj6535@gmail.com>
2023-09-21 17:01:34 +0900

    feat: Add Row component

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useEffect, useState, useCallback } from "react";
+import axios from "../api/axios";
+import "./Row.css";
 
-const Row = () => {
-  return <div>Row</div>;
+const Row = ({ title, id, fetchUrl }) => {
+  const [movies, setMovies] = useState([]);
+  const [handleClick, setHandleClick] = useState(false);
+
+  const fetchMovieData = useCallback(async () => {
+    const res = await axios.get(fetchUrl);
+    // console.log(res);
+    setMovies(res.data.results);
+  }, [fetchUrl]);
+
+  useEffect(() => {
+    fetchMovieData();
+  }, [fetchMovieData]);
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider__arrow-left">
+          <span className="arrow">{"<"}</span>
+        </div>
+        <div id={id} className="row__posters">
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className="row__poster"
+              src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+              alt={movie.name}
+              onClick={() => handleClick(movie)}
+            />
+          ))}
+        </div>
+        <div className="slider__arrow-right">
+          <span className="arrow">{">"}</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Row;
